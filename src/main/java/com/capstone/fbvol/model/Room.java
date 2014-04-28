384987b644960b029258418d9239e9828a3a7a45
SangTae <tkdxo0624@gmail.com>
2014-04-28 17:06:00 +0900

    Making rooming

--- src/main/java/com/capstone/fbvol/model/Room.java
+++ src/main/java/com/capstone/fbvol/model/Room.java
@@ -18,30 +18,35 @@ import java.util.HashMap;
  */
 public class Room {
 
-    private User master;
-    private User rival;
-    private ArrayList<User> observers;
+    private String master;
+    private String rival;
+    private String master_isReady;
+    private String rival_isReady;
+
+    private ArrayList<String> observers;
     private String room_id;
     private String room_title;
-    private boolean isLock;
+    private String isLock;
     private String password;
-    private int runningTime;
-    private boolean roomState; // waiting or playing
-
+    private String runningTime;
+    private String isPlaying; // waiting or playing
 
     ObjectMapper om = new ObjectMapper();
 
-    public void setFromJSON2(String jsonStr) {
+    public void setRoomFromJSON2(String jsonStr) {
         HashMap<String, String> m;
         try {
             m = om.readValue(jsonStr, new TypeReference<HashMap<String, String>>(){});
 
-            //this.master.setFromJSON2(m.get("MASTER"));
-            //this.isLock = m.get("ISLOCK");  // boolean
-            this.room_id = m.get("ROOM_ID");
+            this.master = m.get("MASTER"); //token 값으로 사용
+            this.room_id = m.get("ROOM_ID");   // 고유값
             this.room_title = m.get("ROOM_TITLE");
+
+            this.isLock = m.get("ISLOCK");
             this.password = m.get("PASSWORD");
 
+            this.runningTime = m.get("TIME");        //플레이시간
+
         } catch (JsonParseException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -53,14 +58,18 @@ public class Room {
             e.printStackTrace();
         }
     }
-    public void joinObserverFromJSON2(String jsonStr) {
+
+    public void joinRoomFromJSON2(String jsonStr) { //{"GUEST": RIVAL or OBSERVER , "TOKEN":토큰값 }   guest
         HashMap<String, String> m;
         try {
             m = om.readValue(jsonStr, new TypeReference<HashMap<String, String>>(){});
-            User observer = new User();
-            //observer.setFromJSON2(m.get("OBSERVER"));
-            this.observers.add(observer);
 
+            if(m.get("GUEST").equals("RIVAL") ){
+                this.rival = m.get("RIVAL"); //token 값 사용
+            }
+            else{
+                this.observers.add(m.get("OBSERVER")); //token 값 사용
+            }
         } catch (JsonParseException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -72,11 +81,24 @@ public class Room {
             e.printStackTrace();
         }
     }
-    public void joinRivalFromJSON2(String jsonStr) {
+
+    public void ReadyFromJSON2(String jsonStr) {         // {"TOKEN" : 토큰값 ,  "ISREADY": TRUE or FALSE}
         HashMap<String, String> m;
         try {
             m = om.readValue(jsonStr, new TypeReference<HashMap<String, String>>(){});
-            //rival.setFromJSON2(m.get("RIVAL"));
+
+            if(this.master.equals(m.get("TOKEN")))
+                    master_isReady = m.get("ISREADY");
+            else
+                rival_isReady = m.get("ISREADY");
+
+
+            if( master_isReady.equals("TRUE") && rival_isReady.equals("TRUE")){
+                isPlaying = "TRUE";
+                master_isReady = "FALSE";
+                rival_isReady = "FALSE";
+            }
+
 
         } catch (JsonParseException e) {
             // TODO Auto-generated catch block
@@ -88,75 +110,6 @@ public class Room {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-    }
-    public boolean isRoomState() {
-        return roomState;
-    }
-
-    public void setRoomState(boolean roomState) {
-        this.roomState = roomState;
-    }
-    public User getRival() {
-        return rival;
-    }
-
-    public void setRival(User rival) {
-        this.rival = rival;
-    }
-    public User getMaster() {
-        return master;
-    }
-
-    public void setMaster(User master) {
-        this.master = master;
-    }
-
-    public ArrayList<User> getObserver() {
-        return observers;
-    }
-
-    public void setObserver(ArrayList<User> observer) {
-        this.observers = observer;
-    }
-
-    public String getRoom_id() {
-        return room_id;
-    }
-
-    public void setRoom_id(String room_id) {
-        this.room_id = room_id;
-    }
-
-    public String getRoom_name() {
-        return room_title;
-    }
-
-    public void setRoom_name(String room_title) {
-        this.room_title = room_title;
-    }
-
-    public boolean isLock() {
-        return isLock;
-    }
-
-    public void setLock(boolean lock) {
-        isLock = lock;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public int getRunningTime() {
-        return runningTime;
-    }
-
-    public void setRunningTime(int runningTime) {
-        this.runningTime = runningTime;
     }
 
 }
